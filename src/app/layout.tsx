cf76b2246ce888399450a808609f9d1c660df138
Uchenna Chiagozie <96476199+officialp123@users.noreply.github.com>
2024-08-20 15:17:12 +0100

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,56 +1,53 @@
-import Script from 'next/script';
-import { Inter } from 'next/font/google';
-import { Metadata } from 'next';
+import Script from "next/script";
+import { Inter } from "next/font/google";
+import { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"; // Correct import from @vercel/analytics/react
 
-import './globals.css';
-import Header from '@/components/layout/header';
-import { Providers } from '@/lib/providers';
-import Footer from '@/components/layout/footer';
+import "./globals.css";
+import Header from "@/components/layout/header";
+import { Providers } from "@/lib/providers";
+import Footer from "@/components/layout/footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
-const title = 'Chiagozie Uchenna | Software Developer From Anambra, Nigeria.';
+const title = "Chiagozie Uchenna | Software Developer | frontend developer";
 const description =
-  'A self-proclaimed designer who specializes in frontend development, from Anambra, Nigeria.';
-const url = 'https://www.linkedin.com/in/chiagozie-uchenna/';
+  "A self-proclaimed designer who specializes in frontend development, from Anambra, Nigeria.";
+const url = "https://www.linkedin.com/in/chiagozie-uchenna/";
 
 export const metadata: Metadata = {
   metadataBase: new URL(url),
   title,
   description,
-  keywords: [
-    'Frontend Developer',
-    'React Developer',
-    'JavaScript Developer',
-  ],
-  creator: 'Chiagozie Uchenna',
+  keywords: ["Frontend Developer", "React Developer", "JavaScript Developer"],
+  creator: "Chiagozie Uchenna",
   themeColor: [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
   openGraph: {
-    type: 'website',
+    type: "website",
     url,
     title,
     description,
     siteName: title,
     images: [
       {
-        url: '/images/open-graph-chiagozie.jpg',
+        url: "/images/open-graph-chiagozie.jpg",
       },
     ],
   },
   twitter: {
-    card: 'summary_large_image',
+    card: "summary_large_image",
     title,
     description,
-    creator: '@Chiagozie',
-    images: '/images/open-graph-chiagozie.jpg',
+    creator: "@Chiagozie",
+    images: "/images/open-graph-chiagozie.jpg",
   },
   icons: {
-    icon: '/photo1.jpg',
-    shortcut: '/photo1.jpg',
-    apple: '/photo1.jpg',
+    icon: "/photo1.jpg",
+    shortcut: "/photo1.jpg",
+    apple: "/photo1.jpg",
   },
 };
 
@@ -86,6 +83,7 @@ export default function RootLayout({
           <main className="flex min-h-screen w-full flex-col">{children}</main>
           <Footer />
         </Providers>
+        <Analytics /> {/* Correctly added Analytics component */}
       </body>
     </html>
   );
--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -1,10 +1,14 @@
-'use client';
+"use client";
 
-import Button from '@/components/general/button';
+import Button from "@/components/general/button";
 
 const DownloadCV = () => {
   return (
-    <Button onClick={() => window?.open('/files/chiagozie-uchenna-cv.pdf', '_blank')}>
+    <Button
+      onClick={() =>
+        window?.open("/files/Chiagozie_Uchenna_Resume_FE.pdf", "_blank")
+      }
+    >
       Download CV
     </Button>
   );
