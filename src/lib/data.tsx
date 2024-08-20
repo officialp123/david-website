12f391540a7fe932a8188fa6795ec1c986fa7f61
Uchenna Chiagozie <96476199+officialp123@users.noreply.github.com>
2024-08-20 09:52:00 +0100

    Update data.tsx

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -126,11 +126,6 @@ export const TECHNOLOGIES: TechDetails[] = [
     url: 'https://www.figma.com/',
   },
   {
-    label: 'WebFlow',
-    logo: LogoWebFlow,
-    url: 'https://webflow.com/',
-  },
-  {
     label: 'Cypress',
     logo: LogoCypress,
     darkModeLogo: LogoCypressLight,
