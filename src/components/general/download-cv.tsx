d99c533d1b813d1e624002910c77d0cc8ffe57e9
Uchenna Chiagozie <96476199+officialp123@users.noreply.github.com>
2024-09-20 20:23:53 +0100

    Update download-cv.tsx

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -4,11 +4,14 @@ import Button from "@/components/general/button";
 
 const DownloadCV = () => {
   return (
-    <Button
-      onClick={() =>
-        window?.open("/files/Chiagozie_Uchenna_senior_frontend_Developer.pdf "_blank")
-      }
-    >
+   <Button
+  onClick={() =>
+    window?.open("/files/Chiagozie_Uchenna_senior_frontend_Developer.pdf", "_blank")
+  }
+>
+  Open PDF
+</Button>
+
       Download CV
     </Button>
   );
